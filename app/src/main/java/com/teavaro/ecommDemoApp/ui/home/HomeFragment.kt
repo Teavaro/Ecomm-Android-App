7d4de48b91c7c5d59b916b746160f70a9891cd21
iranvibo <vicenteiran@gmail.com>
2023-02-24 12:36:25 -0500

    remove unused imports

--- app/src/main/java/com/teavaro/ecommDemoApp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/teavaro/ecommDemoApp/ui/home/HomeFragment.kt
@@ -4,15 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebResourceRequest
-import android.webkit.WebResourceResponse
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.teavaro.ecommDemoApp.R
-import com.teavaro.ecommDemoApp.core.MraidController
 import com.teavaro.ecommDemoApp.core.Store
 import com.teavaro.ecommDemoApp.databinding.FragmentHomeBinding
 import com.teavaro.ecommDemoApp.ui.shop.ShopAdapter
@@ -21,7 +16,6 @@ import com.teavaro.funnelConnect.core.initializer.FunnelConnectSDK
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
 
     // This property is only valid between onCreateView and
     // onDestroyView.
