793b7eee8931805cd0b92aa9a8c5d83edac700be
Ahmad Mahmoud <ams.eng@hotmail.com>
2025-04-08 15:57:48 +0200

    fix local host issue when testing on a physical device

--- app/src/main/java/com/teavaro/ecommDemoApp/debugging/SharedPreferencesServer.kt
+++ app/src/main/java/com/teavaro/ecommDemoApp/debugging/SharedPreferencesServer.kt
@@ -17,12 +17,13 @@ class SharedPreferencesServer(context: Context, prefsFileName: String) : NanoHTT
     override fun serve(session: IHTTPSession): Response {
         val uri = session.uri
         val method = session.method
+        // Serve other endpoints
         return when {
             uri == "/" -> serveHtml() // Serve UI
             uri == "/data" && method == Method.GET -> serveJson() // Serve JSON data
             uri == "/update" && method == Method.POST -> updateValue(session) // Update value
             uri == "/delete" && method == Method.POST -> deleteValue(session)
-            else -> newFixedLengthResponse(Response.Status.NOT_FOUND, "text/plain", "404 Not Found")
+            else -> this.createResponse(Response.Status.NOT_FOUND, "text/plain", "404 Not Found")
         }
     }
 
@@ -34,13 +35,13 @@ class SharedPreferencesServer(context: Context, prefsFileName: String) : NanoHTT
         } catch (e: Exception) {
             return newFixedLengthResponse(Response.Status.INTERNAL_ERROR, "text/plain", "Error loading HTML file: ${e.message}")
         }
-        return newFixedLengthResponse(Response.Status.OK, "text/html", html)
+        return this.createResponse(Response.Status.OK, "text/html", html)
     }
 
     private fun serveJson(): Response {
         val allPrefs = prefs.all
         val json = JSONObject(allPrefs).toString(4)
-        return newFixedLengthResponse(Response.Status.OK, "application/json", json)
+        return this.createResponse(Response.Status.OK, "application/json", json)
     }
 
     private fun updateValue(session: IHTTPSession): Response {
@@ -55,16 +56,16 @@ class SharedPreferencesServer(context: Context, prefsFileName: String) : NanoHTT
                 } ?: run {
                     prefs.edit { putString(key, value) }
                 }
-                return newFixedLengthResponse(Response.Status.OK, "text/plain", "Updated")
+                return this.createResponse(Response.Status.OK, "text/plain", "Updated")
             } else {
                 // If key is not found, return a Bad Request response
                 println("❌ Key or value not found in request!")
-                return newFixedLengthResponse(Response.Status.BAD_REQUEST, "text/plain", "Invalid key or value")
+                return this.createResponse(Response.Status.BAD_REQUEST, "text/plain", "Invalid key or value")
             }
         } catch (e: Exception) {
             // Handle errors that might occur during parsing
             println("⚠️ Error parsing the request body: ${e.message}")
-            return newFixedLengthResponse(Response.Status.BAD_REQUEST, "text/plain", "Error updating value: ${e.message}")
+            return this.createResponse(Response.Status.BAD_REQUEST, "text/plain", "Error updating value: ${e.message}")
         }
     }
 
@@ -78,18 +79,20 @@ class SharedPreferencesServer(context: Context, prefsFileName: String) : NanoHTT
             if (key != null) {
                 // Proceed with deletion
                 prefs.edit { remove(key) }
-                return newFixedLengthResponse(Response.Status.OK, "text/plain", "Deleted")
+                return this.createResponse(Response.Status.OK, "text/plain", "Deleted")
             } else {
                 // If key is not found, return a Bad Request response
                 println("❌ Key not found in request!")
-                return newFixedLengthResponse(Response.Status.BAD_REQUEST, "text/plain", "Key not found")
+                return this.createResponse(Response.Status.BAD_REQUEST, "text/plain", "Key not found")
             }
         } catch (e: Exception) {
             // Handle any exceptions that occur while parsing the body
             println("⚠️ Error parsing the request body: ${e.message}")
-            return newFixedLengthResponse(Response.Status.BAD_REQUEST, "text/plain", "Bad Request")
+            return this.createResponse(Response.Status.BAD_REQUEST, "text/plain", "Bad Request")
         }
     }
+
+    private fun createResponse(status: Response. IStatus, mimeType: String, message: String) = newFixedLengthResponse(status, mimeType, message)
 
     companion object {
 
@@ -99,7 +102,6 @@ class SharedPreferencesServer(context: Context, prefsFileName: String) : NanoHTT
             this.server = SharedPreferencesServer(context, prefsFileName)
             try {
                 this.server.start()
-                Timber.tag("SharedPreferencesServer").d("Server started at http://localhost:8080")
             } catch (e: IOException) {
                 e.printStackTrace()
             }
